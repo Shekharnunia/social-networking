b8bab798ae7c9a4411c2389958ad73087a2d3459
Shekhar <shekharnunia@gmail.com>
2019-06-09 15:53:52 +0530

    urls has been connected with main url file and app added into settings

--- src/socialnetwork/settings.py
+++ src/socialnetwork/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
+    'plans',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
