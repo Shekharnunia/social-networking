a185c07e8dab8ba1050325c063b568650107d834
Shekhar <shekharnunia@gmail.com>
2019-06-09 15:54:53 +0530

    models has been created for status, comments and plans

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from plans.models import Plan
 
-# Create your models here.
+
+class Status(models.Model):
+    content = models.TextField()
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    plan = models.ManyToManyField(Plan)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='status_like')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class StatusComment(models.Model):
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=250)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
