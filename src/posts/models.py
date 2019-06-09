7c7d9c5f2f4aec81be3646e03a3081bd770fe4d5
Shekhar <shekharnunia@gmail.com>
2019-06-09 21:44:23 +0530

    changes made in status model field and migrations are made

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,16 +1,21 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
+
 from plans.models import Plan
 
 
 class Status(models.Model):
     content = models.TextField()
-    like = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='status_like')
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
+
+    def get_absolute_url(self):
+        return reverse('posts:list')
 
 
 class StatusComment(models.Model):
