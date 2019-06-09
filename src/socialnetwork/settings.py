a3d922cd949d601e6142c74e216944c89ed48567
Shekhar <shekharnunia@gmail.com>
2019-06-09 16:31:23 +0530

    base template added with login and logout tempaltes, login and logout redirect url added in settings

--- src/socialnetwork/settings.py
+++ src/socialnetwork/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'posts:home'
+LOGOUT_REDIRECT_URL = 'login'
